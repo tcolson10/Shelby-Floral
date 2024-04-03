01b288fc8518da2785e72f1b09fec3a7890fd1de
tcolson10 <tcolson10@gmail.com>
2024-04-02 22:53:39 -0600

    array deletion

--- src/pages/Pricing.js
+++ src/pages/Pricing.js
@@ -1,47 +1,50 @@
 import React from 'react';
 
-// Updated array of pricing details
-const pricingData = [
-    {
-        src: "/images/portfolio/kylie1.JPG",
-        alt: "Bridal Party",
-        description: ["Bridal Bouquet $175-250", "Bridesmaid Bouquet $65+", "Boutonniere $18", "Corsage $20", "", "For other contact me for price"]
-    },
-    {
-        src: "/images/portfolio/tree.JPG",
-        alt: "Installations",
-        description: ["Arch Piece $350+", "Full Arch $1,000+", "Broken Arch $1,000+", "Aisle Florals $100+", "", "For other contact me for price"]
-    },
-    {
-        src: "/images/portfolio/ceremony.JPG",
-        alt: "Reception",
-        description: ["Centerpieces $50+", "Welcome Sign Florals $40+", "Cake florals $25+", "Cake meadow $75+", "Bud Vases $7 with rented vases $5 without vase", "", "For other please contact me"]
-    }
-];
-
 function Pricing() {
     return (
         <div className="pricing-page-container">
             <div className="pricing-header" style={{ textAlign: 'center' }}>
                 <h1 className='page-title'>Pricing</h1>
-                <p>Pricing varies based upon the types of flowers <br />To get an exact quote please contact me</p>
+                <p>Pricing varies based upon the types of flowers<br />To get an exact quote please contact me</p>
             </div>
 
             <div className="pricing-container">
-                {pricingData.map((item, index) => (
-                    <div key={index} className="pricing-photo-container">
-                        <img src={item.src} alt={item.alt} className="pricing-photo" />
-                        <div className="pricing-text-content"> {/* Additional div for text content if needed */}
-                            <h3>{item.alt}</h3>
-                            {item.description.map((line, lineIndex) => (
-                                <React.Fragment key={lineIndex}>
-                                    {lineIndex > 0 && <br />} {/* Adds breaks between lines */}
-                                    {line}
-                                </React.Fragment>
-                            ))}
-                        </div>
+                <div className="pricing-photo-container">
+                    <img src="/images/portfolio/kylie1.JPG" alt="Bridal Party" className="pricing-photo" />
+                    <div className="pricing-text-content">
+                        <h3>Bridal Party</h3>
+                        Bridal Bouquet $175-250<br />
+                        Bridesmaid Bouquet $65+<br />
+                        Boutonniere $18<br />
+                        Corsage $20<br />
+                        <br />For other contact me for price
                     </div>
-                ))}
+                </div>
+
+                <div className="pricing-photo-container">
+                    <img src="/images/portfolio/tree.JPG" alt="Installations" className="pricing-photo" />
+                    <div className="pricing-text-content">
+                        <h3>Installations</h3>
+                        Arch Piece $350+<br />
+                        Full Arch $1,000+<br />
+                        Broken Arch $1,000+<br />
+                        Aisle Florals $100+<br />
+                        <br />For other contact me for price
+                    </div>
+                </div>
+
+                <div className="pricing-photo-container">
+                    <img src="/images/portfolio/ceremony.JPG" alt="Reception" className="pricing-photo" />
+                    <div className="pricing-text-content">
+                        <h3>Reception</h3>
+                        Centerpieces $50+<br />
+                        Welcome Sign Florals $40+<br />
+                        Cake florals $25+<br />
+                        Cake meadow $75+<br />
+                        Bud Vases $7 with rented vases $5 without vase<br />
+                        <br />For other please contact me
+                    </div>
+                </div>
             </div>
         </div>
     );
