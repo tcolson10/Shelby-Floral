98c8b1c76ccced0c96e1a93e8168a16376c9606b
tcolson10 <tcolson10@gmail.com>
2024-04-14 21:37:52 -0600

    Analytics2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link, Outlet, useLocation } from 'react-router-dom';
-import ReactGA from 'react-ga';
+import ReactGA from 'react-ga4';
 import './App.css';
 import './index.css';
 
